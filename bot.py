1b30e9e0531ef8123a329245464a5c864e349f5c
ulrikelorenz15-cyber <ulrikelorenz15@gmail.com>
2026-04-08 04:40:01 +0200

    bot.py aktualisieren

--- bot.py
+++ bot.py
@@ -273,4 +273,5 @@ def home_route():
 if __name__ == "__main__":
     webhook_url = os.getenv("WEBHOOK_URL")
     requests.get(f"{URL}/setWebhook?url={webhook_url}/webhook/{TOKEN}")
-    app.run(host="0.0.0.0", port=8080)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
